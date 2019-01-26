bfbdefd0adf04734ef8ce76b70bd100151e39a09
Noureddine Saidana <noureddine.wow@laposte.net>
2019-01-26 18:09:44 +0100

    working on actor page

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,35 +1,11 @@
 import React, {Component} from 'react'
-import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import axios from 'axios';
-import films_helper from './../helpers/films_helper'
-import { ListItemText } from '@material-ui/core';
-
-const styles = {
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-};
 
 class Films extends Component {
     constructor(props) {
@@ -38,16 +14,6 @@ class Films extends Component {
         this.state = {
             films: [],
         }
-
-        
-    }
-
-    getFilms2 = async (e) => {
-        films_helper( (results) => {
-            this.setState({
-                films: results
-            })
-        })
     }
 
     getFilms = (e) => {
@@ -82,10 +48,8 @@ class Films extends Component {
                                         </CardContent>
                                         <CardActions>
                                             <Button href="/film/{index}" style={{margin:'auto'}}>Learn More</Button>
-                                            {/* <Link to="/film/{index}">Learn again</Link> */}
                                         </CardActions>
-                                    </Card>
-                                    
+                                    </Card>  
                                 )
                             )}   
                         </List>
